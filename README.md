e5bef0d44f2989868bc72000205964eb9ba67b64
Linus Färnstrand <faern@faern.net>
2018-10-14 01:39:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Rust GBG - CLI workshop
+
+## Getting started
+
+1. Clone this repository.
+1. Install Rust: Go to rustup.rs and follow instructions.
+1. Install and make the latest nightly toolchain the default. Because we will use the
+   soon-to-be-released Rust 2018 edition!
+   ```bash
+   rustup default nightly
+   ```
+1. Install the code formatter. This can later be invoked with `cargo fmt`.
+   ```bash
+   rustup component add rustfmt-preview
+   ```
+1. Try building this project
+   ```bash
+   cargo build
+   ```
+1. Try running this project
+   ```bash
+   cargo run -- --help
+   cargo run
+   cargo run -- --delay 1000 /tmp
+   ```
+
+## Exercises
+
+Do these if you want to. Otherwise do whatever you want to the program. Or create a completely new
+CLI tool.
+
+1. Extend the program to allow filtering out which type of events it prints.
+1. The `println!` macro has to lock stdout for each call to the macro. This is not good for
+   performance. Try locking stdout in the beginning of the program and make it use that.
+1. Make the program take an `-o <path>` argument and write the same there as to stdout.
+
+## The Rust CLI working group
+
+The Rust community has a number of "working groups". Each focus on a different area and tries to
+improve Rust in that area. The CLI working group can be found over at
+https://github.com/rust-lang-nursery/cli-wg
+
+More specifically, *their* tutorial on getting started writing a CLI app is located at 
+https://github.com/rust-lang-nursery/cli-wg/blob/master/src/tutorial/README.md
+
+## More inspiration
+
+There are quite a lot of useful CLI tools written in Rust. Here I will mention a few that tries to
+be replacements for very common tools. It's likely that you will already know approximately
+what they do.
+
+* `ripgrep` - Insanely fast `grep` replacement. You will never want to use `grep` after trying this.
+* `fd` - Fast and user-friendly alternative to `find`.
+* `fselect` - Another `find` alternative. This one has an SQL like syntax for querying.
+* `exa` - A modern replacement for `ls`.
+* `bat` - A `cat` clone, but with syntax highlighting, line numbers and Git integration.
---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use notify::Watcher;
+use std::{env, path::PathBuf, sync::mpsc, time::Duration};
+
+fn main() {
+    let settings = parse_arguments();
+
+    // Set up the filesystem watcher. Events will be send back over the `event_rx` channel receiver.
+    let (event_tx, event_rx) = mpsc::channel();
+    let mut watcher = notify::watcher(event_tx, settings.delay).unwrap();
+    watcher
+        .watch(settings.watch_path, notify::RecursiveMode::Recursive)
+        .unwrap();
+
+    for event in event_rx {
+        use notify::DebouncedEvent::*;
+        match event {
+            NoticeWrite(_path) => (),
+            NoticeRemove(path) => (),
+            Create(path) => println!("create {}", path.display()),
+            Write(path) => println!("write {}", path.display()),
+            Chmod(path) => println!("chmod {}", path.display()),
+            Remove(path) => println!("remove {}", path.display()),
+            Rename(from, to) => println!("rename {} => {}", from.display(), to.display()),
+            Rescan => (),
+            Error(error, None) => eprintln!("error: {}", error),
+            Error(error, Some(path)) => eprintln!("error at {}: {}", path.display(), error),
+        }
+    }
+    // Since we want to listen to events until the user exits with ctrl-c, it's always an error to
+    // reach the end of the program.
+    eprintln!("Notify has crashed");
+    ::std::process::exit(1);
+}
+
+
+struct Settings {
+    watch_path: PathBuf,
+    delay: Duration,
+}
+
+/// Uses the `clap` crate to generate help/usage printing as well as parse the given arguments.
+fn parse_arguments() -> Settings {
+    let matches = clap::App::new(clap::crate_name!())
+        .version(clap::crate_version!())
+        .author(clap::crate_authors!())
+        .about(clap::crate_description!())
+        .arg(
+            clap::Arg::with_name("PATH")
+                .help("The path to watch. Uses current working directory if not specified")
+                .index(1),
+        )
+        .arg(
+            clap::Arg::with_name("delay")
+                .help("Set the event delay in ms. Helps group chained events into single events")
+                .short("d")
+                .long("delay")
+                .default_value("100"),
+        )
+        .get_matches();
+
+    // Pull out the PATH argument. Fall back to the current working directory if it was not given.
+    let watch_path = match matches.value_of_os("PATH") {
+        Some(path) => PathBuf::from(path),
+        None => env::current_dir().unwrap(),
+    };
+
+    // Get the delay value and try to parse it into an u64. If this fails clap will print an error
+    // and make the program exit.
+    let delay_ms = clap::value_t!(matches.value_of("delay"), u64).unwrap_or_else(|e| e.exit());
+    let delay = Duration::from_millis(delay_ms);
+
+    Settings { watch_path, delay }
+}
